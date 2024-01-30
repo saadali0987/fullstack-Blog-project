dcd70832ff536604f9a0b4f3354865344c6a02f6
saadali0987 <slh098098@gmail.com>
2024-01-30 22:56:45 +0500

    Added functionality to the sign up page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,12 +4,10 @@ import { errorHandler } from "../utils/error.js"
 
 export const signup = async(req,res, next) => {
     const {username, email, password} = req.body
-
     if(!username || !email || !password || username === '' || email === '' || password === '')
     {
         next(errorHandler(400, "All field are required"))
     }
-
     const hashedPassword = bcrypt.hashSync(password, 10);
 
     const newUser = new User({
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,53 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Signup = () => {
+  const [formData, setFormData] = useState({})
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!formData.username || !formData.email || !formData.password) {
+      setErrorMessage("Please fill out all fields")
+    }
+    else {
+      try {
+        setLoading(true)
+        setErrorMessage(null)
+        const res = await fetch("/api/auth/signup", {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
+        })
+        const data = await res.json()
+        if(data.success === "false")
+        {
+          return setErrorMessage(data.message)
+        }
+        setLoading(false)
+        if(res.ok)
+        {
+          navigate('/signin')
+        }
+      } catch (err) {
+        setErrorMessage(err.message)
+        setLoading(false)
+      }
+    }
+
+  }
+
+
+
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='p-3 mx-auto flex flex-col md:flex-row md:items-center max-w-3xl gap-10'>
@@ -17,39 +62,54 @@ const Signup = () => {
 
         {/* right side */}
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value='Your username' />
               <TextInput
                 type='text'
                 placeholder='yourName123'
                 id='username'
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value='Your email' />
               <TextInput
-                type='text'
+                type='email'
                 placeholder='name@company.com'
                 id='email'
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value='Your password' />
               <TextInput
-                type='text'
+                type='password'
                 placeholder='Password'
                 id='password'
+                onChange={handleChange}
               />
             </div>
 
-            <Button className='text-bold text-white' gradientDuoTone="purpleToPink" type='submit' >Sign Up</Button>
+            <Button disabled={loading} className='text-bold text-white' gradientDuoTone="purpleToPink" type='submit' >
+              {loading ? (
+                <div>
+                  <Spinner size="sm"/>  
+                  <span className='pl-3'>Loading...</span>
+                </div>
+                
+              ) : 'Sign Up'}
+            </Button>
           </form>
 
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
             <Link className='text-blue-500' to="/signin">Sign In</Link>
           </div>
+
+          {errorMessage && (
+            <Alert className='mt-5' color="failure">{errorMessage}</Alert>
+          )}
         </div>
       </div>
     </div>
