e946304b9e30a52c0bcf8d79d46da99fccdf85d0
saadali0987 <slh098098@gmail.com>
2024-01-30 16:46:13 +0500

    installed react and tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-3xl'>hello world</div>
+  )
+}
+
+export default App
