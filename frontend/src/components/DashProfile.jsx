ff5a17e7f0ab9e5336e479048f322897c51af957
saadali0987 <slh098098@gmail.com>
2024-02-01 18:51:32 +0500

    profile section UI done

--- frontend/src/components/DashProfile.jsx
+++ frontend/src/components/DashProfile.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import {TextInput, Label, Button} from 'flowbite-react'
 
 const DashProfile = () => {
-  return (
-    <div>DashProfile</div>
-  )
+    const { currentUser } = useSelector(state => state.user)
+    return (
+        <div className='max-w-lg mx-auto p-3 w-full'>
+            {/* <h1 className='text-center my-3 font-semibold text-3xl'>Profile</h1> */}
+            <form className='flex flex-col gap-4 my-5'>
+                <div className='w-28 h-28 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
+                    <img src={currentUser.profilePicture} alt="user" className='rounded-full object-cover w-full h-full border-4 border-[lightgray]' />
+                </div>
+
+                <div className='italic'>
+                    <div className="mb-2 block">
+                        <Label htmlFor="username" value="Your username:" />
+                    </div>
+                    <TextInput type='text' id='username' placeholder="username" defaultValue={currentUser.username} />
+                </div>
+
+                <div className='italic'>
+                    <div className="mb-2 block">
+                        <Label htmlFor="email" value="Your email:" />
+                    </div>
+                    <TextInput type='email' id='email' placeholder="email" defaultValue={currentUser.email} />
+                </div>
+
+                <div className='italic'>
+                    <div className="mb-2 block">
+                        <Label htmlFor="password" value="Your password:" />
+                    </div>
+                    <TextInput type='password' id='password' placeholder="password" />
+                </div>
+
+                <Button className='mt-3' type='submit' gradientDuoTone='purpleToBlue' outline>Update</Button>
+            </form>
+
+            <div className='text-red-500 flex justify-end mt-4 text-sm'>
+                <span className='underline'>*Delete Account</span>
+            </div>
+        </div>
+    )
 }
 
 export default DashProfile
